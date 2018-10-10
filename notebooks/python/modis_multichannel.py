3b1741bc7599098b88494ec6294cb64781af5623
Phil Austin <paustin@eos.ubc.ca>
2018-10-10 11:47:21 -0600

    wed oct 10 correction

--- notebooks/python/modis_multichannel.py
+++ notebooks/python/modis_multichannel.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # <h1>Table of Contents<span class="tocSkip"></span></h1>
-# <div class="toc"><ul class="toc-item"><li><span><a href="#Changes-to-modis_level1b_read" data-toc-modified-id="Changes-to-modis_level1b_read-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Changes to modis_level1b_read</a></span></li><li><span><a href="#Introduction" data-toc-modified-id="Introduction-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Introduction</a></span></li><li><span><a href="#Reading-modis-data" data-toc-modified-id="Reading-modis-data-3"><span class="toc-item-num">3&nbsp;&nbsp;</span>Reading modis data</a></span><ul class="toc-item"><li><span><a href="#Using-pydf-to-get-metadata" data-toc-modified-id="Using-pydf-to-get-metadata-3.1"><span class="toc-item-num">3.1&nbsp;&nbsp;</span>Using pydf to get metadata</a></span></li><li><span><a href="#open-one-of-the-datasets-(number-4,-EV_1KM_Emissive)-and-get-its-shape-and-data-type" data-toc-modified-id="open-one-of-the-datasets-(number-4,-EV_1KM_Emissive)-and-get-its-shape-and-data-type-3.2"><span class="toc-item-num">3.2&nbsp;&nbsp;</span>open one of the datasets (number 4, EV_1KM_Emissive) and get its shape and data type</a></span></li><li><span><a href="#Get-the-first-row-of-the-first-channel-and-find-its-numpy-dtype" data-toc-modified-id="Get-the-first-row-of-the-first-channel-and-find-its-numpy-dtype-3.3"><span class="toc-item-num">3.3&nbsp;&nbsp;</span>Get the first row of the first channel and find its numpy dtype</a></span></li><li><span><a href="#get-all-the-rows-and-columns-for-the-first-channel" data-toc-modified-id="get-all-the-rows-and-columns-for-the-first-channel-3.4"><span class="toc-item-num">3.4&nbsp;&nbsp;</span>get all the rows and columns for the first channel</a></span></li><li><span><a href="#Find-the-attributes-for-EV_1KM_Emissive" data-toc-modified-id="Find-the-attributes-for-EV_1KM_Emissive-3.5"><span class="toc-item-num">3.5&nbsp;&nbsp;</span>Find the attributes for EV_1KM_Emissive</a></span></li><li><span><a href="#Print-the-first-100-characters-of-the-Metadata.0-string" data-toc-modified-id="Print-the-first-100-characters-of-the-Metadata.0-string-3.6"><span class="toc-item-num">3.6&nbsp;&nbsp;</span>Print the first 100 characters of the Metadata.0 string</a></span></li></ul></li><li><span><a href="#Now-write-channels-out-into-a-new-hdf" data-toc-modified-id="Now-write-channels-out-into-a-new-hdf-4"><span class="toc-item-num">4&nbsp;&nbsp;</span>Now write channels out into a new hdf</a></span><ul class="toc-item"><li><span><a href="#find-the-index-for-channel-30" data-toc-modified-id="find-the-index-for-channel-30-4.1"><span class="toc-item-num">4.1&nbsp;&nbsp;</span>find the index for channel 30</a></span></li><li><span><a href="#Let-python-figure-this-out" data-toc-modified-id="Let-python-figure-this-out-4.2"><span class="toc-item-num">4.2&nbsp;&nbsp;</span>Let python figure this out</a></span></li><li><span><a href="#Read-channel-30-at-index-9-into-a-numpy-array-of-type-uint16" data-toc-modified-id="Read-channel-30-at-index-9-into-a-numpy-array-of-type-uint16-4.3"><span class="toc-item-num">4.3&nbsp;&nbsp;</span>Read channel 30 at index 9 into a numpy array of type uint16</a></span></li></ul></li><li><span><a href="#Calibrate-the-raw-counts-for-both-channels" data-toc-modified-id="Calibrate-the-raw-counts-for-both-channels-5"><span class="toc-item-num">5&nbsp;&nbsp;</span>Calibrate the raw counts for both channels</a></span></li><li><span><a href="#Write-the-calibrated-channel-out-for-safekeeping" data-toc-modified-id="Write-the-calibrated-channel-out-for-safekeeping-6"><span class="toc-item-num">6&nbsp;&nbsp;</span>Write the calibrated channel out for safekeeping</a></span></li></ul></div>
+# <div class="toc"><ul class="toc-item"><li><span><a href="#Changes-to-modis_level1b_read" data-toc-modified-id="Changes-to-modis_level1b_read-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Changes to modis_level1b_read</a></span></li><li><span><a href="#Introduction" data-toc-modified-id="Introduction-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Introduction</a></span></li><li><span><a href="#If-you-want-to-work-with-my-day-222:" data-toc-modified-id="If-you-want-to-work-with-my-day-222:-3"><span class="toc-item-num">3&nbsp;&nbsp;</span>If you want to work with my day 222:</a></span></li><li><span><a href="#Reading-modis-data" data-toc-modified-id="Reading-modis-data-4"><span class="toc-item-num">4&nbsp;&nbsp;</span>Reading modis data</a></span><ul class="toc-item"><li><span><a href="#Using-pydf-to-get-metadata" data-toc-modified-id="Using-pydf-to-get-metadata-4.1"><span class="toc-item-num">4.1&nbsp;&nbsp;</span>Using pydf to get metadata</a></span></li><li><span><a href="#open-one-of-the-datasets-(number-4,-EV_1KM_Emissive)-and-get-its-shape-and-data-type" data-toc-modified-id="open-one-of-the-datasets-(number-4,-EV_1KM_Emissive)-and-get-its-shape-and-data-type-4.2"><span class="toc-item-num">4.2&nbsp;&nbsp;</span>open one of the datasets (number 4, EV_1KM_Emissive) and get its shape and data type</a></span></li><li><span><a href="#Get-the-first-row-of-the-first-channel-and-find-its-numpy-dtype" data-toc-modified-id="Get-the-first-row-of-the-first-channel-and-find-its-numpy-dtype-4.3"><span class="toc-item-num">4.3&nbsp;&nbsp;</span>Get the first row of the first channel and find its numpy dtype</a></span></li><li><span><a href="#get-all-the-rows-and-columns-for-the-first-channel" data-toc-modified-id="get-all-the-rows-and-columns-for-the-first-channel-4.4"><span class="toc-item-num">4.4&nbsp;&nbsp;</span>get all the rows and columns for the first channel</a></span></li><li><span><a href="#Find-the-attributes-for-EV_1KM_Emissive" data-toc-modified-id="Find-the-attributes-for-EV_1KM_Emissive-4.5"><span class="toc-item-num">4.5&nbsp;&nbsp;</span>Find the attributes for EV_1KM_Emissive</a></span></li><li><span><a href="#Print-the-first-100-characters-of-the-Metadata.0-string" data-toc-modified-id="Print-the-first-100-characters-of-the-Metadata.0-string-4.6"><span class="toc-item-num">4.6&nbsp;&nbsp;</span>Print the first 100 characters of the Metadata.0 string</a></span></li></ul></li><li><span><a href="#Now-write-channels-out-into-a-new-hdf" data-toc-modified-id="Now-write-channels-out-into-a-new-hdf-5"><span class="toc-item-num">5&nbsp;&nbsp;</span>Now write channels out into a new hdf</a></span><ul class="toc-item"><li><span><a href="#find-the-index-for-channel-30" data-toc-modified-id="find-the-index-for-channel-30-5.1"><span class="toc-item-num">5.1&nbsp;&nbsp;</span>find the index for channel 30</a></span></li><li><span><a href="#Let-python-figure-this-out" data-toc-modified-id="Let-python-figure-this-out-5.2"><span class="toc-item-num">5.2&nbsp;&nbsp;</span>Let python figure this out</a></span></li><li><span><a href="#Read-channel-30-at-index-9-into-a-numpy-array-of-type-uint16" data-toc-modified-id="Read-channel-30-at-index-9-into-a-numpy-array-of-type-uint16-5.3"><span class="toc-item-num">5.3&nbsp;&nbsp;</span>Read channel 30 at index 9 into a numpy array of type uint16</a></span></li></ul></li><li><span><a href="#Calibrate-the-raw-counts-for-both-channels" data-toc-modified-id="Calibrate-the-raw-counts-for-both-channels-6"><span class="toc-item-num">6&nbsp;&nbsp;</span>Calibrate the raw counts for both channels</a></span></li><li><span><a href="#Write-the-calibrated-channel-out-for-safekeeping" data-toc-modified-id="Write-the-calibrated-channel-out-for-safekeeping-7"><span class="toc-item-num">7&nbsp;&nbsp;</span>Write the calibrated channel out for safekeeping</a></span></li></ul></div>
 
 # # Changes to modis_level1b_read
 # 
@@ -44,17 +44,37 @@ import pdb
 # 
 #      
 
+# # If you want to work with my day 222:
+# 
+# execute the cell below
+# otherwise delete
+
 # In[2]:
 
-
-import pprint
 
 #
 # this is my MYD02 file, substitute your own here
 # It needs to be copyied to a301.data_dir
 #
 modis_file="MYD021KM.A2013222.2105.061.2018047235850.hdf"
+download(modis_file)
 generic_m2 = a301.data_dir / Path(modis_file)
+shutil.copy(Path(modis_file),generic_m2)
+
+
+# In[3]:
+
+
+#
+# replace with your file name if needed
+#
+modis_file="MYD021KM.A2013222.2105.061.2018047235850.hdf"
+generic_m2 = a301.data_dir / Path(modis_file)
+
+
+# In[4]:
+
+
 #
 # confirm that we can read this file
 #
@@ -82,7 +102,7 @@ generic_rad= a301.data_dir / Path('rad_file_2018_10_1.hdf')
 # written before pathlib (which was introduced in python 3.5), 
 # so I need to convert the Path object to a simple string using str()
 
-# In[3]:
+# In[5]:
 
 
 the_file = SD(str(generic_m2), SDC.READ)
@@ -95,7 +115,7 @@ help(SD.info)
 
 # ## open one of the datasets (number 4, EV_1KM_Emissive) and get its shape and data type
 
-# In[4]:
+# In[6]:
 
 
 longwave_data = the_file.select('EV_1KM_Emissive') # select sds
@@ -106,7 +126,7 @@ print(longwave_data.info())
 # 
 # (unit16 is "unsigned 16 bit integer", which is how the modis raw counts are stored)
 
-# In[5]:
+# In[7]:
 
 
 data_row = longwave_data[0,0,:] # get sds data
@@ -115,7 +135,7 @@ print(data_row.shape,data_row.dtype)
 
 # ## get all the rows and columns for the first channel
 
-# In[6]:
+# In[8]:
 
 
 longwave_data[0,:,:]
@@ -123,7 +143,7 @@ longwave_data[0,:,:]
 
 # ## Find the attributes for EV_1KM_Emissive
 
-# In[7]:
+# In[9]:
 
 
 pprint.pprint(longwave_data.attributes() )
@@ -133,7 +153,7 @@ pprint.pprint(longwave_data.attributes() )
 # 
 # Date, orbit number, etc. are stored in a long string attribute called 'StructMetadata.0'.  The \t character is a tab stop.
 
-# In[8]:
+# In[10]:
 
 
 pprint.pprint(the_file.attributes()['StructMetadata.0'][:100])
@@ -141,13 +161,13 @@ pprint.pprint(the_file.attributes()['StructMetadata.0'][:100])
 
 # # Now write channels out into a new hdf
 
-# In[9]:
+# In[11]:
 
 
 longwave_bands = the_file.select('Band_1KM_Emissive')
 
 
-# In[10]:
+# In[12]:
 
 
 longwave_bands.attributes()
@@ -159,7 +179,7 @@ longwave_bands.attributes()
 # 
 # Count the following and convince yourself that channel 30 is index 9, starting from 0
 
-# In[11]:
+# In[13]:
 
 
 band_nums=longwave_bands.get()
@@ -173,7 +193,7 @@ print(f'here are the modis channels in the emissive dataset \n{band_nums}')
 # We need to turn that index (type int64) into a plain python int so it can be used to specify the channel
 # (float doesn't work)
 
-# In[12]:
+# In[14]:
 
 
 def get_index(band_nums,chan_num):
@@ -208,7 +228,7 @@ print(f'channel 30 is located at index {ch30_index} and channel 31 at {ch31_inde
 
 # ## Read channel 30 at index 9 into a numpy array of type uint16
 
-# In[13]:
+# In[15]:
 
 
 ch30_data = longwave_data[ch30_index,:,:]
@@ -228,7 +248,7 @@ print(ch30_data.dtype)
 # 
 # 
 
-# In[14]:
+# In[16]:
 
 
 scales=longwave_data.attributes()['radiance_scales']
@@ -241,7 +261,7 @@ print(f'ch30 scale: {ch30_scale}, ch30 offset: {ch30_offset}')
 print(f'ch31 scale: {ch31_scale}, ch31 offset: {ch31_offset}')
 
 
-# In[15]:
+# In[17]:
 
 
 ch30_calibrated =(ch30_data - ch30_offset)*ch30_scale
@@ -253,7 +273,7 @@ the_file.end()
 # 
 # Follow the example here: https://hdfeos.org/software/pyhdf.php
 
-# In[16]:
+# In[18]:
 
 
 def write_chan(sd,numpy_array,chan_name):
@@ -272,6 +292,7 @@ def write_chan(sd,numpy_array,chan_name):
 
     # Write data
     sds[:,:] = numpy_array[:,:]
+    print(f'writing sds with shape {numpy_array.shape}')
 
     # Close the dataset
     sds.endaccess()
@@ -291,7 +312,7 @@ sdout.comment="written by modis_multichannel.ipynb"
 sdout.end()
 
 
-# In[17]:
+# In[19]:
 
 
 from a301.scripts import hdf4ls
