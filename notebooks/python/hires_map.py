187a8515742bf9447428e6a415b713392540cd32
Phil Austin <paustin@eos.ubc.ca>
2018-11-24 13:09:49 -0800

    coastlines working

---
+++ notebooks/python/hires_map.py
@@ -0,0 +1,135 @@
+
+# coding: utf-8
+
+# <h1>Table of Contents<span class="tocSkip"></span></h1>
+# <div class="toc"><ul class="toc-item"><li><span><a href="#Introduction" data-toc-modified-id="Introduction-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Introduction</a></span></li><li><span><a href="#Read-the-geotiff-with-rasterio" data-toc-modified-id="Read-the-geotiff-with-rasterio-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Read the geotiff with rasterio</a></span></li><li><span><a href="#Locate-UBC-on-the-map" data-toc-modified-id="Locate-UBC-on-the-map-3"><span class="toc-item-num">3&nbsp;&nbsp;</span>Locate UBC on the map</a></span></li><li><span><a href="#Higher-resolution-coastline" data-toc-modified-id="Higher-resolution-coastline-4"><span class="toc-item-num">4&nbsp;&nbsp;</span>Higher resolution coastline</a></span><ul class="toc-item"><li><span><a href="#Mapped-image-with-no-coastline" data-toc-modified-id="Mapped-image-with-no-coastline-4.1"><span class="toc-item-num">4.1&nbsp;&nbsp;</span>Mapped image with no coastline</a></span></li><li><span><a href="#Read-the-shape-file-and-add-the-coastline-to-the-image" data-toc-modified-id="Read-the-shape-file-and-add-the-coastline-to-the-image-4.2"><span class="toc-item-num">4.2&nbsp;&nbsp;</span>Read the shape file and add the coastline to the image</a></span></li></ul></li></ul></div>
+
+# # Introduction
+# 
+# Below we read in the small Vancouver image we wrote out in the image_zoom notebook, and put it on a map with a UTM-10N crs.  We then add a high resolution coastline read from the openstreetmap coastline database.
+
+# In[1]:
+
+
+import rasterio
+import a301
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.colors import Normalize
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import cartopy
+from rasterio.windows import Window
+from pyproj import transform as proj_transform
+from pyproj import Proj
+from a301.landsat.toa_reflectance import toa_reflectance_8
+import pprint
+from a301.utils.data_read import download
+from pathlib import Path
+from affine import Affine
+
+
+# # Read the geotiff with rasterio
+
+# In[2]:
+
+
+tif_filename=a301.data_dir / Path('small_file.tiff')    
+with rasterio.open(tif_filename) as raster:
+    affine_transform=raster.transform
+    crs=raster.crs
+    profile=raster.profile
+    refl=raster.read(1)
+plt.hist(refl[~np.isnan(refl)].flat)
+plt.title('band 5 reflectance for Vancouver section');
+
+
+# In[3]:
+
+
+print(f"profile: \n{pprint.pformat(profile)}")
+
+
+# # Locate UBC on the map
+# 
+# We need to project the center of campus from lon/lat to UTM 10N x,y using pyproj.transform
+
+# In[4]:
+
+
+p_utm = Proj(crs)
+p_latlon=Proj(proj='latlong',datum='WGS84')
+ubc_lon = -123.2460
+ubc_lat = 49.2606
+ubc_x, ubc_y =proj_transform(p_latlon,p_utm,ubc_lon, ubc_lat) 
+height, width = refl.shape
+ubc_ul_xy = affine_transform*(0,0)
+ubc_lr_xy = affine_transform*(width,height)
+ubc_ul_xy,ubc_lr_xy
+
+
+# # Higher resolution coastline
+
+# Here is what Point Grey looks like with the [open street maps](https://automating-gis-processes.github.io/2017/lessons/L7/retrieve-osm-data.html) coastline database.
+# 
+# Optional: If you want to do this for your own image your're going to need to reduce the size of the coastlines database.  There is a good article about different sources for map data on the blog [python4oceanographers](
+# https://ocefpaf.github.io/python4oceanographers/blog/2015/06/22/osm/).  The basic steps that worked for me:
+# 
+# 1. Download the 513 Mbyte shape file of the WGS84 coastline database from [openstreetmap](http://openstreetmapdata.com/data/coastlines)
+# 
+# 2. Unzipping the file (it will be about 700 Mbytes) will create a folder called 
+#    coastlines-split-4326  (4326 is the epsg number for WGS84 lon/lat)
+#    
+# 3. Figure out the lon/lat coordinates of a bounding box that contains your scene
+# 
+# 4. Get a fiona prompt, which provides the command line program ogr2ogr
+#    (ogr stands for"OpenGIS Simple Features Reference Implementation"). For Vancouver, I used this command (all one line, lons are negative, lats are positive).  Substitute your own lons and lats (note all - signs are single, not double hyphens)
+#    
+#        ogr2ogr -skipfailures -f "ESRI Shapefile"  -clipsrc -123.5 49 -123.1 49.4   ubc_coastlines coastlines-split-4326
+#        
+#    this extracts the segments and writes them to a new  folder called [ubc_coastlines](https://github.com/phaustin/a301_code/tree/master/test_data/ubc_coastlines) which is less than 140 K and which provides the coastlines below.
+# 
+# 
+# 
+
+# ## Mapped image with no coastline
+# 
+# Sanity check to make sure we've got the right image
+
+# In[5]:
+
+
+vmin=0.0
+vmax=0.5
+the_norm=Normalize(vmin=vmin,vmax=vmax,clip=False)
+palette='viridis'
+pal = plt.get_cmap(palette)
+pal.set_bad('0.75') #75% grey for out-of-map cells
+pal.set_over('w')  #color cells > vmax red
+pal.set_under('k')  #color cells < vmin black
+cartopy_crs=cartopy.crs.epsg(crs.to_epsg())
+fig, ax = plt.subplots(1, 1,figsize=[15,25],
+                       subplot_kw={'projection': cartopy_crs})
+image_extent=[ubc_ul_xy[0],ubc_lr_xy[0],ubc_ul_xy[1],ubc_lr_xy[1]]
+ax.imshow(refl,origin="lower",
+         extent=image_extent,transform=cartopy_crs,
+         cmap=pal,norm=the_norm);
+ax.plot(ubc_x, ubc_y,'ro',markersize=25)
+ax.set_extent(image_extent,crs=cartopy_crs)
+
+
+# ## Read the shape file and add the coastline to the image
+# 
+# Note that PlateCarree is another name for WGS84 datum, simple lat/lon which is the projection of the coastlines-split-4326 shapefile.
+
+# In[6]:
+
+
+from cartopy.io import shapereader
+shape_project=cartopy.crs.PlateCarree()
+shp = shapereader.Reader(str(a301.test_dir / 
+                             Path("ubc_coastlines/lines.shp")))
+for record, geometry in zip(shp.records(), shp.geometries()):
+    ax.add_geometries([geometry], shape_project,facecolor="none",
+                      edgecolor='red',lw=2)
+display(fig)
+
---
+++ notebooks/python/image_zoom.py
@@ -0,0 +1,212 @@
+
+# coding: utf-8
+
+# <h1>Table of Contents<span class="tocSkip"></span></h1>
+# <div class="toc"><ul class="toc-item"><li><span><a href="#Introduction" data-toc-modified-id="Introduction-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Introduction</a></span></li><li><span><a href="#Get-the-tiff-files-and-calculate-band-5-reflectance" data-toc-modified-id="Get-the-tiff-files-and-calculate-band-5-reflectance-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Get the tiff files and calculate band 5 reflectance</a></span></li><li><span><a href="#Save-the-crs-and-the-full_affine" data-toc-modified-id="Save-the-crs-and-the-full_affine-3"><span class="toc-item-num">3&nbsp;&nbsp;</span>Save the crs and the full_affine</a></span></li><li><span><a href="#Locate-UBC-on-the-map" data-toc-modified-id="Locate-UBC-on-the-map-4"><span class="toc-item-num">4&nbsp;&nbsp;</span>Locate UBC on the map</a></span></li><li><span><a href="#Locate-UBC-on-the-image" data-toc-modified-id="Locate-UBC-on-the-image-5"><span class="toc-item-num">5&nbsp;&nbsp;</span>Locate UBC on the image</a></span></li><li><span><a href="#make-our-subscene-400-pixels-wide-and-600-pixels-tall,-using-UBC-as-a-reference-point" data-toc-modified-id="make-our-subscene-400-pixels-wide-and-600-pixels-tall,-using-UBC-as-a-reference-point-6"><span class="toc-item-num">6&nbsp;&nbsp;</span>make our subscene 400 pixels wide and 600 pixels tall, using UBC as a reference point</a></span></li><li><span><a href="#Plot-the-raw-band-5-image,-clipped-to-reflectivities-below-0.6" data-toc-modified-id="Plot-the-raw-band-5-image,-clipped-to-reflectivities-below-0.6-7"><span class="toc-item-num">7&nbsp;&nbsp;</span>Plot the raw band 5 image, clipped to reflectivities below 0.6</a></span></li><li><span><a href="#put-this-on-a-map" data-toc-modified-id="put-this-on-a-map-8"><span class="toc-item-num">8&nbsp;&nbsp;</span>put this on a map</a></span></li><li><span><a href="#Use--rasterio--to-write-a-new-tiff-file" data-toc-modified-id="Use--rasterio--to-write-a-new-tiff-file-9"><span class="toc-item-num">9&nbsp;&nbsp;</span>Use  rasterio  to write a new tiff file</a></span><ul class="toc-item"><li><span><a href="#Set-the-affine-transform-for-the-scene" data-toc-modified-id="Set-the-affine-transform-for-the-scene-9.1"><span class="toc-item-num">9.1&nbsp;&nbsp;</span>Set the affine transform for the scene</a></span></li><li><span><a href="#Now-write-this-out-to-small_file.tiff" data-toc-modified-id="Now-write-this-out-to-small_file.tiff-9.2"><span class="toc-item-num">9.2&nbsp;&nbsp;</span>Now write this out to small_file.tiff</a></span></li></ul></li></ul></div>
+
+# # Introduction
+# 
+# We need to be able to select a small region of a landsat image to work with.  This notebook 
+# 
+# 1. zooms in on a 400 pixel wide x 600 pixel high subscene centered on  Vancouver,  using pyproj and the affine transform to map from lat,lon to x,y in UTM zone 10N to row, column in the landsat image
+# 
+# 2. Puts on crude coastlines in the UTM-10N crs
+# 
+# 3. Calculates the new affine transform for the subcene, and writes the image out to a 1 Mbyte tiff file
+# 
+
+# In[1]:
+
+
+import rasterio
+import a301
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.colors import Normalize
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import cartopy
+from rasterio.windows import Window
+from pyproj import transform as proj_transform
+from pyproj import Proj
+from a301.landsat.toa_reflectance import toa_reflectance_8
+import pprint
+from a301.utils.data_read import download
+from pathlib import Path
+from affine import Affine
+
+filenames=["LC08_L1TP_047026_20150614_20180131_01_T1_B4.TIF",
+    "LC08_L1TP_047026_20150614_20180131_01_T1_B5.TIF",
+    "LC08_L1TP_047026_20150614_20180131_01_T1_MTL.txt"]
+dest_folder=a301.data_dir / Path("landsat8/vancouver")
+for the_file in filenames:
+    landsat_tif = Path('landsat_scenes/l8_vancouver') / Path(the_file)
+    download(str(landsat_tif),dest_folder=dest_folder)
+
+
+# # Get the tiff files and calculate band 5 reflectance
+
+# In[2]:
+
+
+band4=list(dest_folder.glob("*_B4.TIF"))[0]
+band5=list(dest_folder.glob("*_B5.TIF"))[0]
+mtl_file=list(dest_folder.glob("*MTL.txt"))[0]
+
+
+# # Save the crs and the full_affine
+# 
+# We need to keep both full_affine (the affine transform for the full scene, and the coordinate reference system for pyproj (called crs below)
+
+# In[3]:
+
+
+with rasterio.open(band5) as b5_raster:
+    full_affine=b5_raster.transform
+    crs=b5_raster.crs
+    full_profile=b5_raster.profile
+    refl=toa_reflectance_8([5],mtl_file)
+    b5_refl=refl[5]
+plt.hist(b5_refl[~np.isnan(b5_refl)].flat)
+plt.title('band 5 reflectance whole scene')
+
+
+# In[4]:
+
+
+print(f"profile: \n{pprint.pformat(full_profile)}")
+
+
+# # Locate UBC on the map
+# 
+# We need to project the center of campus from lon/lat to UTM 10N x,y using pyproj_transform and our crs (which in this case is UTM).  We can transform from lon,lat (p_lonlat) to x,y (p_utm) to anchor us to a known point in the map coordinates.
+
+# In[5]:
+
+
+p_utm = Proj(crs)
+p_lonlat=Proj(proj='latlong',datum='WGS84')
+ubc_lon = -123.2460
+ubc_lat = 49.2606
+ubc_x, ubc_y =proj_transform(p_lonlat,p_utm,ubc_lon, ubc_lat) 
+
+
+# # Locate UBC on the image
+# 
+# Now we need to use the affine transform to go between x,y and 
+# col, row on the image.  The next cell creates two slice objects that extend  on either side of the center point.  The tilde (~) in front of the transform indicates that we're going from x,y to col,row, instead of col,row to x,y.  (See [this blog entry](http://www.perrygeo.com/python-affine-transforms.html) for reference.)  Remember that row 0 is the top row, with rows decreasing downward to the south.  To demonstrate, the cell below uses the tranform to calculate the x,y coordinates of the (0,0) corner.
+
+# In[6]:
+
+
+full_ul_xy=np.array(full_affine*(0,0))
+print(f"orig ul corner x,y (km)={full_ul_xy*1.e-3}")
+
+
+# # make our subscene 400 pixels wide and 600 pixels tall, using UBC as a reference point
+# 
+# We need to find the right rows and columns on the image to save for the subscene.  Do this by working outward from UBC by a certain number of pixels in each direction, using the inverse of the full_affine transform to go from x,y to col,row
+
+# In[7]:
+
+
+ubc_col, ubc_row = ~full_affine*(ubc_x,ubc_y)
+ubc_col, ubc_row = int(ubc_col), int(ubc_row)
+l_col_offset= -100
+r_col_offset= +300
+b_row_offset= +100
+t_row_offset= -500
+col_slice=slice(ubc_col+l_col_offset,ubc_col+r_col_offset)
+row_slice=slice(ubc_row + t_row_offset, ubc_row + b_row_offset)
+section=b5_refl[row_slice,col_slice]
+ubc_ul_xy = full_affine*(col_slice.start,row_slice.start)
+ubc_lr_xy = full_affine*(col_slice.stop,row_slice.stop)
+ubc_ul_xy,ubc_lr_xy
+
+
+# # Plot the raw band 5 image, clipped to reflectivities below 0.6
+# 
+# Note that the origin is "upper" because the (0,0) pixel is the upper
+# left corner.  This is a simple check that we got the right section.
+
+# In[8]:
+
+
+vmin=0.0
+vmax=0.6
+the_norm=Normalize(vmin=vmin,vmax=vmax,clip=False)
+palette='viridis'
+pal = plt.get_cmap(palette)
+pal.set_bad('0.75') #75% grey for out-of-map cells
+pal.set_over('w')  #color cells > vmax red
+pal.set_under('k')  #color cells < vmin black
+fig, ax = plt.subplots(1,1,figsize=(15,25))
+ax.imshow(section,cmap=pal,norm=the_norm,origin="upper");
+
+
+# # put this on a map
+# 
+# Note that the origin is switched to "lower" in the x,y coordinate system below,
+# since y increases upwards.  The coastline is very crude, but at least indicates we've got the coords roughly correct.  See the "high_res_map" notebook for a better coastline.
+
+# In[9]:
+
+
+cartopy_crs=cartopy.crs.epsg(crs.to_epsg())
+fig, ax = plt.subplots(1, 1,figsize=[15,25],
+                       subplot_kw={'projection': cartopy_crs})
+image_extent=[ubc_ul_xy[0],ubc_lr_xy[0],ubc_ul_xy[1],ubc_lr_xy[1]]
+ax.imshow(section,cmap=pal,norm=the_norm,origin="lower",
+         extent=image_extent,transform=cartopy_crs,alpha=0.8);
+ax.coastlines(resolution='10m',color='red',lw=2);
+ax.plot(ubc_x, ubc_y,'ro',markersize=25)
+ax.set_extent(image_extent,crs=cartopy_crs)
+
+
+# # Use  rasterio  to write a new tiff file
+# 
+# ## Set the affine transform for the scene
+# 
+# We can write this clipped image back out to a much smaller tiff file if we can come up with the new affine transform for the smaller scene.  Referring again [to the writeup](http://www.perrygeo.com/python-affine-transforms.html) we need:
+# 
+#     a = width of a pixel
+#     b = row rotation (typically zero)
+#     c = x-coordinate of the upper-left corner of the upper-left pixel
+#     d = column rotation (typically zero)
+#     e = height of a pixel (typically negative)
+#     f = y-coordinate of the of the upper-left corner of the upper-left pixel
+# 
+# which will gives:
+# 
+# new_affine=Affine(a,b,c,d,e,f)
+# 
+# In addition, need to add a third dimension to the section array, because
+# rasterio expects [band,x,y] for its writer.  Do this with np.newaxis in the next cell
+
+# In[20]:
+
+
+image_height, image_width = section.shape
+ul_x, ul_y = ubc_ul_xy[0], ubc_ul_xy[1]
+new_affine=(30.,0.,ul_x,0.,-30.,ul_y)
+out_section=section[np.newaxis,...]
+print(out_section.shape)
+
+
+# ##  Now write this out to small_file.tiff
+# 
+# See the hires_map notebook for how to read this in and plot it
+
+# In[11]:
+
+
+tif_filename=a301.data_dir / Path('small_file.tiff')    
+num_chans=1
+with rasterio.open(tif_filename,'w',driver='GTiff',
+                   height=image_height,width=image_width,
+                   count=num_chans,dtype=out_section.dtype,
+                   crs=crs,transform=new_affine, nodata=0.0) as dst:
+        dst.write(out_section)
+        section_profile=dst.profile
+        
+print(f"section profile: {pprint.pformat(section_profile)}")
+
